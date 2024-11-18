02dd484b49fb0a47e456142540537e2682771dfd
Allan Gori <ogollogori@gmail.com>
2024-11-18 20:41:43 +0300

    minor spelling updates

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,7 +1,7 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 def get_books_by_author(author_name):
-    books = Book.objects.filter(author__name=author_name)
+    books = Author.objects.filter(name=author_name)
     return books
 
 def get_books_in_library(library_name):
