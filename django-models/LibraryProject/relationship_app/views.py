2dc3ae3bce83121fc1bd9cda224ad2829323cc92
Allan Gori <ogollogori@gmail.com>
2024-11-19 11:16:36 +0300

    task three for creating role-based access control

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,26 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResponseForbidden
+
+def check_role(role):
+    def decorator(user):
+        return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == role
+    return decorator
+
+@user_passes_test(check_role('Admin'))
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(check_role('Librarian'))
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(check_role('Member'))
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
+
 
 class LoginView(LoginView):
     template_name = 'login.html'
@@ -23,9 +43,7 @@ def register(request):
             return redirect('login')
     else:
         form = UserCreationForm()
-    return render(request, 'relationship_app/register.html', {'form': form})
-
-
+    return render(request, 'relationship_app/templates/relationship_app/register.html', {'form': form})
 
 
 def list_books(request):
@@ -36,5 +54,3 @@ class LibraryDetailView(DetailView):
     model =  Library
     template_name = 'relationship_app/templates/relationship_app/library_detail.html'
     context_object_name = 'library'
-
-'relationship_app/register.html'
