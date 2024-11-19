e1fa6a91f3e55514ce8085c616469d208d89a47c
Allan Gori <ogollogori@gmail.com>
2024-11-19 08:34:17 +0300

    new small function updates

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,9 +1,10 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 def get_books_by_author(author,  author_name):
-    books = Author.objects.get(name=author_name),
-    objects.filter(author = author)
+    books = Author.objects.get(name=author_name)
     return books
+def get_books_by_author(author):
+    return Book.objects.filter(author=author)
 
 def get_books_in_library(library_name):
     library = Library.objects.get(name=library_name)
