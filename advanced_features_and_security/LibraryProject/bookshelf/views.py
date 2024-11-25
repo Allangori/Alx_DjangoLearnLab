6b100596da42ae43a85cd2d3953f851dfccf88db
root <ogolloallan@gmail.com>
2024-11-25 08:37:53 +0300

    updated views.py with cutom permissions

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,10 +1,47 @@
 from typing import Any
-from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseForbidden
 from django.views.generic import TemplateView, DetailView, UpdateView
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book
-from django.contrib.auth.decorators import login_required, user_passes_test
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def add_book(request):
+    if request.method == "POST":
+        title = request.POST['title']
+        author = request.POST['author']
+        published_date = request.POST['published_date']
+        isbn = request.POST['isbn']
+        Book.objects.create(title=title, author=author, published_date=published_date, isbn=isbn)
+        return redirect('book_list')
+    return render(request, 'bookshelf/add_book.html')
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == "POST":
+        book.title = request.POST['title']
+        book.author = request.POST['author']
+        book.published_date = request.POST['published_date']
+        book.isbn = request.POST['isbn']
+        book.save()
+        return redirect('book_list')
+    return render(request, 'bookshelf/edit_book.html', {'book': book})
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == "POST":
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'bookshelf/delete_book.html', {'book': book})
+
 
 
 class BookUpdateView(UpdateView):
