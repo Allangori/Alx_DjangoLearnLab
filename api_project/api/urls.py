7be6deecf27a74892786c68c66d9d1472cec2b62
root <ogolloallan@gmail.com>
2024-11-26 11:00:01 +0300

    updated permissions and authentication

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import BookList
+from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet, BookList  # Import your views
 
@@ -8,6 +8,8 @@ router.register(r'books_all', BookViewSet, basename='book_all')  # Register the 
 
 
 urlpatterns = [
+    path('auth/token/', obtain_auth_token, name='obtain-token'),
+    path('books/', BookList.as_view(), name='book-list'),
     path('', include(router.urls)),
-    path('books/', BookList.as_view(), name='book-list')
+    
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated, BasePermission
 # from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
@@ -7,7 +8,16 @@ from .serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+#class IsAdminOrReadOnly(BasePermission):
+    #def has_permission(self, request, view):
+        #if request.method in ['GET', 'HEAD', 'OPTIONS']:
+            #return True
+        #return request.user and request.user.is_staff
+#permission_classes = [IsAdminOrReadOnly]
