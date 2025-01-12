facc74d18e95fd77c18127683228484d5657c057
root <ogolloallan@gmail.com>
2025-01-12 23:03:12 +0300

    minor changes

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -17,6 +17,6 @@ urlpatterns = [
     path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
     #path('posts/<int:post_id>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
     path('post/<int:pk>/comments/new/', views.CommentCreateView.as_view(), name='comment-create'),
-    path('comments/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment-update'),
-    path('comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
+    path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='comment-update'),
+    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
 ]
